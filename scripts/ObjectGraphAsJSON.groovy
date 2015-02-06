d9a0fcbc9ba6e60fdd0acb292299d8a87276b0c6
John Flinchbaugh <jflinchbaugh@hmc.psu.edu>
2015-02-06 09:03:21 -0500

    script to show off JsonBuilder for quick printlns

---
+++ scripts/ObjectGraphAsJSON.groovy
@@ -0,0 +1,49 @@
+/**
+ * Want a "nice", complete println of a graph of objects without
+ * having to traverse each object by hand.  Use the JsonBuilder.
+ */
+import groovy.json.*
+
+// just some setup stuff
+class Person {
+    String name
+    List<Address> addresses
+}
+
+class Address {
+    AddressType type
+    String line1
+    String line2
+    String city
+    String state
+    String postalCode
+}
+
+enum AddressType {
+    HOME,
+    WORK,
+    EMERGENCY
+}
+
+def person = new Person(
+    name: 'john', 
+    addresses: [
+        new Address(
+            type: AddressType.HOME,
+            line1: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            postalCode: '11011'
+        ),
+        new Address(
+            type: AddressType.WORK,
+            line1: '1234 Back Alley',
+            city: 'New York',
+            state: 'NY',
+            postalCode: '11011'
+        )
+    ]
+)
+
+// the useful code!
+println new JsonBuilder(person).toPrettyString()
