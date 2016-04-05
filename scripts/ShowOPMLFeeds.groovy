6f71a3da7cd3604f6b7ff66a88de8c6b3d3202a3
John Flinchbaugh <jflinchbaugh@hmc.psu.edu>
2016-04-05 11:14:13 -0400

    a sample script for parsing and displaying text version of opml

---
+++ scripts/ShowOPMLFeeds.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+// open an OPML file and print out a readable text rendering of it
+
+def opml = new XmlSlurper().parse("BeyondPodFeeds.opml")
+println opml.body.outline.each { cat ->
+    if (! cat.outline.size()) {
+        return
+    }
+    println cat.@text
+    cat.outline.each { feed ->
+        println "  ${feed.@text} - ${feed.@xmlUrl}"
+    }
+}
