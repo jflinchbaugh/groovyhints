7bd5bcd2ad20ac7e30b892ba4cc531ca5ac6e4fb
John Flinchbaugh <jflinchbaugh@hmc.psu.edu>
2017-04-03 15:22:24 -0400

    doc updates

--- scripts/gebsh.groovy
+++ scripts/gebsh.groovy
@@ -1,16 +1,10 @@
 /**
  * This is an extension script to boot up Geb and a browser
  * to facilitate interactively exploring a page with Geb.
+ * It will likely require a webdriver for your browser:
+ * Geckodriver for firefox: https://github.com/mozilla/geckodriver/releases
+ *
  * % groovysh gebsh.groovy
- * groovy:000> go 'http://google.com'
- * groovy:000> println q
- * groovy:000> println $('a')*.@href
- * groovy:000> $('a')[0].click()
- * groovy:000> refresh() // to update current state from browser
- * groovy:000> println title
- * groovy:000> println currentUrl
- * groovy:000> println page.title
- * groovy:000> driver.navigate().back()
  */
 import groovy.grape.Grape
 
@@ -20,6 +14,13 @@ Grape.grab(
     module: 'selenium-firefox-driver',
     version: '3.3.1'
 )
+/*
+Grape.grab(
+    group: 'org.seleniumhq.selenium',
+    module: 'htmlunit-driver',
+    version: '2.25'
+)
+*/
 
 import geb.*
 import org.openqa.selenium.*
@@ -37,3 +38,17 @@ this.'$' = browser.&$
 go = { Object[] args -> def ret = browser.go(*args); refresh(); ret }
 
 refresh()
+
+println '''
+Welcome!
+Example Usage:
+  groovy:000> go 'http://google.com'
+  groovy:000> println $('a')*.@href
+  groovy:000> $('a')[0].click()
+  groovy:000> refresh() // to update current state from browser
+  groovy:000> println title
+  groovy:000> println currentUrl
+  groovy:000> println page.title
+  groovy:000> driver.navigate().back()
+  groovy:000> println browser.q
+'''
