fda4580a186363d02ba8fd6a22101e21fab75a35
John Flinchbaugh <john@hjsoft.com>
2018-01-05 18:37:27 -0500

    filter before iterating

--- scripts/ShowOPMLFeeds.groovy
+++ scripts/ShowOPMLFeeds.groovy
@@ -3,10 +3,9 @@
 // open an OPML file and print out a readable text rendering of it
 
 def opml = new XmlSlurper().parse("BeyondPodFeeds.opml")
-println opml.body.outline.each { cat ->
-    if (! cat.outline.size()) {
-        return
-    }
+println opml.body.outline.findAll {
+    it.outline.size()
+}.each { cat ->
     println cat.@text
     cat.outline.each { feed ->
         println "  ${feed.@text} - ${feed.@xmlUrl}"
